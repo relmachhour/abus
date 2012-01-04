4195b46021a7a774bd321be2d27150f6a0c27e48
fillods <fillods@gmail.com>
2012-01-04 14:32:39 +0000

    example-svc-attr: add auto changing attribute spewing events

--- examples/example-svc-attr.c
+++ examples/example-svc-attr.c
@@ -23,23 +23,36 @@
 int main(int argc, char **argv)
 {
 	abus_t abus;
+	const char *servicename = "exampleattrsvc";
+	int i;
 
 	int my_int = 42;
 	int my_other_int = -2;
+	int my_auto_count = 0;
 
 
 	abus_init(&abus);
 
-	abus_decl_attr_int(&abus, "exampleattrsvc", "tree.some_int", &my_int,
+	abus_decl_attr_int(&abus, servicename, "tree.some_int", &my_int,
 					ABUS_RPC_FLAG_NONE,
 					"Some integer, for demo purpose");
 
-	abus_decl_attr_int(&abus, "exampleattrsvc", "tree.some_other_int", &my_other_int,
+	abus_decl_attr_int(&abus, servicename, "tree.some_other_int", &my_other_int,
 					ABUS_RPC_FLAG_NONE,
 					"Some other integer, still for demo purpose");
 
+	abus_decl_attr_int(&abus, servicename, "tree.auto_count", &my_auto_count,
+					ABUS_RPC_FLAG_NONE,
+					"Counter incremented every 5 seconds");
+
 	/* do other stuff */
-	sleep(10000);
+	for (i = 0; i < 1000; i++) {
+		sleep(5);
+
+		my_auto_count++;
+		/* trigger event notification */
+		abus_attr_changed(&abus, servicename, "tree.auto_count");
+	}
 
 	abus_cleanup(&abus);
 
