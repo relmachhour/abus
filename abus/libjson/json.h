0409940ebfa8f56e2e193a7be1e4e046fead9c0c
Thierry.Gayet <Thierry.Gayet@gmail.com>
2012-02-02 10:07:02 +0000

    Add a wrapper for the libjson.
    This is done for extracting data from a single json file.

--- abus/libjson/json.h
+++ abus/libjson/json.h
@@ -20,6 +20,7 @@ extern "C" {
 
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdbool.h>
 
 #if defined(_MSC_VER) && (_MSC_VER < 1600)
 // MSVC does not include stdint.h before version 10.0
@@ -226,6 +227,140 @@ int json_parser_dom_free(json_parser_dom *ctx);
 
 /** helper to parser callback that arrange parsing events into comprehensive JSON data structure */
 int json_parser_dom_callback(void *userdata, int type, const char *data, size_t length);
+
+// -------------------------------------------------------------------------------------
+// @brief  Wrapper used for extracting data from a single json file
+// @author Thierry GAYET
+// @date   January 2012                
+// -------------------------------------------------------------------------------------
+
+struct json_val_elem
+{
+	char*						key;
+	uint32_t 					key_length;
+	struct json_dom_val*		val;
+};
+
+typedef struct json_dom_val
+{
+	int 						type;
+	int 						length;
+	union
+	{
+		char*					data;
+		struct json_dom_val**		array;
+		struct json_val_elem**	object;
+	} u;
+} json_dom_val_t;
+
+/**
+ * @brief  Function that load and analyse a json file
+ * @param  path + json file to parse
+ * @return dom like of the json file
+ */
+json_dom_val_t* json_config_open(const char* szJsonFilename);
+
+/**
+ * @brief  Function that clean a json file
+ * @param  json object
+ * @return none
+ */
+void json_config_cleanup(json_dom_val_t* element);
+
+/**
+ * @brief  Function that locate a main directory
+ * @param  pointer to the main json dom
+ * @return pointer to the element that contain
+ */
+json_dom_val_t* json_config_lookup(json_dom_val_t* element, const char* szDirectoryNane);
+
+/*
+ * @brief Extract the integer value from a data
+ *
+ * @param current item
+ * @param pointer to the integer value expected
+ *
+ * @return integer value
+ */
+int json_config_get_int(json_dom_val_t* element, int* val);
+
+/*
+ * @brief Extract the integer value from a data
+ *
+ * @param pointer to the dom
+ * @param directory name
+ * @param attribute's name
+ * @paral the integer value expected
+ *
+ * @return return code
+ */
+int json_config_get_direct_int(json_dom_val_t* root, const char* directoryName, const char* attributeName, int* val);
+
+/*
+ * @brief Extract the boolean value from a data
+ *
+ * @param current item
+ * @param pointer to the boolean value expected
+ *
+ * @return boolean value
+ */
+int json_config_get_bool(json_dom_val_t* element, bool* val);
+
+/*
+ * @brief Extract the boolean value from a data
+ *
+ * @param pointer to the dom
+ * @param directory name
+ * @param attribute's name
+ * @paral the boolean value expected
+ *
+ * @return return code
+ */
+bool json_config_get_direct_bool(json_dom_val_t* root, const char* directoryName, const char* attributeName, bool* val);
+
+/*
+ * @brief Extract the string value from a data
+ *
+ * @param current item
+ * @param pointer to the string value expected
+ *
+ * @return string value
+ */
+int json_config_get_string(json_dom_val_t* element, char** val);
+
+/*
+ * @brief Extract the string value from a data
+ *
+ * @param pointer to the dom
+ * @param directory name
+ * @param attribute's name
+ * @paral the string value expected
+ *
+ * @return return code
+ */
+int json_config_get_direct_string(json_dom_val_t* root, const char* directoryName, const char* attributeName, char** val);
+
+/*
+ * @brief Extract the double value from a data
+ *
+ * @param current item
+ * @param pointer to the double value expected
+ *
+ * @return return code
+ */
+int json_config_get_double(json_dom_val_t* element, double* val);
+
+/*
+ * @brief Extract the double value from a data
+ *
+ * @param pointer to the dom
+ * @param directory name
+ * @param attribute's name
+ * @paral the double value expected
+ *
+ * @return return code
+ */
+int json_config_get_direct_double(json_dom_val_t* root, const char* directoryName, const char* attributeName, double* val);
 
 #ifdef __cplusplus
 }
